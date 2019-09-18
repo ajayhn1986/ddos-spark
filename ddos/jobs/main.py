dad6e7a652d9dd3ed7be778aa4fa43fe946ffcb6
Ajay <ajaynh@microland>
2019-09-18 15:33:49 +0530

    changed operations to ingest/detect

--- ddos/jobs/main.py
+++ ddos/jobs/main.py
@@ -19,8 +19,8 @@ def parseArgs():
             "  Command: main.py <applicationPropertiesFilePath> <operation: load/parse>")
     else:
         args["configFilePath"] = sys.argv[1]
-        if sys.argv[2] != 'load' and sys.argv[2] != 'parse':
-            raise Exception("Invalid operation specified, accepts only load/parse")
+        if sys.argv[2] != 'ingest' and sys.argv[2] != 'detect':
+            raise Exception("Invalid operation specified, accepts only ingest/detect")
         args["operation"] = sys.argv[2]
     return args
 
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         config = getConfig(args.get("configFilePath"))
         validateConfig(config)
         spark = SparkSession.builder.appName("ddos").master("local[*]").getOrCreate()
-        if args.get("operation") == 'load':
+        if args.get("operation") == 'ingest':
             parsedDf = pRun(spark, config.get("srcFilePath"))
             writeDFToKafka(parsedDf, config.get("kafkaBootstrapServers"), config.get("kafkaTopic"))
         else:
@@ -54,4 +54,4 @@ if __name__ == '__main__':
             writeDFToFile(finalDf, config.get("targetPath"))
     except Exception as e:
         print("Exception: {}".format(str(e)))
-    #spark-submit --jars libs/spark-sql-kafka-0-10_2.11-2.4.0.jar,libs/kafka-clients-0.10.2.2.jar --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=config/log4j-spark.properties" --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=config/log4j-spark.properties" --py-files jobs.zip jobs/main.py C:/Users/ajaynh/Downloads/phData-Assignment/ddos/config/application_properties.json load/parse
+    #spark-submit --jars libs/spark-sql-kafka-0-10_2.11-2.4.0.jar,libs/kafka-clients-0.10.2.2.jar --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=config/log4j-spark.properties" --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=config/log4j-spark.properties" --py-files jobs.zip jobs/main.py C:/Users/ajaynh/Downloads/phData-Assignment/ddos/config/application_properties.json ingest/detect
